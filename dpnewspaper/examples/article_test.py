7541f15ef14316bc7d51d8e4e628359a6dab2b27
Lousm <ying0229._>
2020-09-08 17:15:03 +0800

    更新版本信息

--- dpnewspaper/examples/article_test.py
+++ dpnewspaper/examples/article_test.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #-*- encoding: utf-8 -*-
 #
-# Copyright (c) 2020 daypop.ai, Inc. All Rights Reserved
+# Copyright (c) 2020 LouShimin, Inc. All Rights Reserved
 #
 # Version : 1.0
-# Author  : luoyingbo@daypop.ai
+# Author  : LouShimin
 # Time    : 2020/4/18 15:35
 # Desc    :   
 
